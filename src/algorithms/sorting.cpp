a8a29612f346445d3d6cf0a4683377af0b55e8e3
Anders Eriksson <aeri3@kth.se>
2019-07-31 18:47:39 +0200

    minor cleanup

--- src/algorithms/sorting.cpp
+++ src/algorithms/sorting.cpp
@@ -4,34 +4,33 @@
 
 namespace sorting {
 
-void bubble_sort(int arr[], int n)
-{
-  bool sorted = false;
-  while (!sorted) {
-    sorted = true;
-    for (int i = 0; i < n - 1; i++) {
-      if (arr[i] > arr[i + 1]) {
-        int tmp = arr[i];
-        arr[i] = arr[i + 1];
-        arr[i + 1] = tmp;
-        sorted = false;
+  void bubble_sort(int arr[], int n)
+  {
+    bool sorted = false;
+    while (!sorted) {
+      sorted = true;
+      for (int i = 0; i < n - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+          int tmp = arr[i];
+          arr[i] = arr[i + 1];
+          arr[i + 1] = tmp;
+          sorted = false;
+        }
       }
     }
   }
-}
 
-void insertion_sort(int arr[], int n)
-{
-  for (int i = 1; i < n; i++) {
-    int key = arr[i];
-    int j = i - 1;
-    while (j >= 0 && arr[j] > key) {
-      arr[j + 1] = arr[j];
-      j = j - 1;
+  void insertion_sort(int arr[], int n)
+  {
+    for (int i = 1; i < n; i++) {
+      int key = arr[i];
+      int j = i - 1;
+      while (j >= 0 && arr[j] > key) {
+        arr[j + 1] = arr[j];
+        j = j - 1;
+      }
+      arr[j + 1] = key;
     }
-    arr[j + 1] = key;
   }
-}
 
 }
-
--- src/algorithms/sorting.h
+++ src/algorithms/sorting.h
@@ -2,10 +2,8 @@
 #define SORTING_H
 
 namespace sorting {
-
   void bubble_sort(int arr[], int n);
   void insertion_sort(int arr[], int n);
-
 }
 
 #endif // !SORTING_H
