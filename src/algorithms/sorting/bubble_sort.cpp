ce93512250760f0283188408399ba7cc692c4abd
Anders Eriksson <aeri3@kth.se>
2020-01-13 15:10:48 +0100

    finish sorting test fixture and bubble sort test

--- src/algorithms/sorting/bubble_sort.cpp
+++ src/algorithms/sorting/bubble_sort.cpp
@@ -1,19 +1,16 @@
 #include "bubble_sort.h"
 
-namespace sorting {
-
-  void bubble_sort(int arr[], int n)
-  {
-    bool sorted = false;
-    while (!sorted) {
-      sorted = true;
-      for (int i = 0; i < n - 1; i++) {
-        if (arr[i] > arr[i + 1]) {
-          int tmp = arr[i];
-          arr[i] = arr[i + 1];
-          arr[i + 1] = tmp;
-          sorted = false;
-        }
+void sorting::bubble_sort(int arr[], int n)
+{
+  bool sorted = false;
+  while (!sorted) {
+    sorted = true;
+    for (int i = 0; i < n - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        int tmp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = tmp;
+        sorted = false;
       }
     }
   }
--- test/util.cpp
+++
@@ -1,7 +0,0 @@
-bool compare_array(int a1[], int a2[], int n)
-{
-  for (int i = n; i < n; i++)
-    if (a1[i] != a2[i])
-      return false;
-  return true;
-}
