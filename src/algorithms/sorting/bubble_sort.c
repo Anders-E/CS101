78e9f95bb04a4c17cbdab064a844e09b732c7c79
Anders Eriksson <aeri3@kth.se>
2020-01-15 15:37:15 +0100

    switch indentation to tabs

--- src/algorithms/sorting/bubble_sort.c
+++ src/algorithms/sorting/bubble_sort.c
@@ -2,16 +2,16 @@
 
 void bubble_sort(int arr[], int n)
 {
-  int sorted = 0;
-  while (!sorted) {
-    sorted = 1;
-    for (int i = 0; i < n - 1; i++) {
-      if (arr[i] > arr[i + 1]) {
-        int tmp = arr[i];
-        arr[i] = arr[i + 1];
-        arr[i + 1] = tmp;
-        sorted = 0;
-      }
-    }
-  }
+	int sorted = 0;
+	while (!sorted) {
+		sorted = 1;
+		for (int i = 0; i < n - 1; i++) {
+			if (arr[i] > arr[i + 1]) {
+				int tmp = arr[i];
+				arr[i] = arr[i + 1];
+				arr[i + 1] = tmp;
+				sorted = 0;
+			}
+		}
+	}
 }
