b26a1218eca9ec1d6a5d28a4dade89755f5d4251
Anders Eriksson <aeri3@kth.se>
2020-01-15 23:32:30 +0100

    minor stylistic fix

--- src/algorithms/sorting/counting_sort.c
+++ src/algorithms/sorting/counting_sort.c
@@ -3,21 +3,20 @@
 
 void counting_sort(int arr[], int n, int k)
 {
-    int *count = calloc(k, sizeof(int));
-    int *sorted = malloc(n * sizeof(int));
+	int *count = calloc(k, sizeof(int));
+	int *sorted = malloc(n * sizeof(int));
 
-    for (int i = 0; i < n; i++)
-        count[arr[i]]++;
-    for (int i = 1; i < k; i++)
-        count[i] += count[i - 1];
-    for (int i = n - 1; i >= 0; i--) {
-        sorted[count[arr[i]] - 1] = arr[i];
-        count[arr[i]]--;
-    }
-    for (int i = 0; i < n; i++) {
-        arr[i] = sorted[i];
-    }
-    
-    free(count);
-    free(sorted);
+	for (int i = 0; i < n; i++)
+		count[arr[i]]++;
+	for (int i = 1; i < k; i++)
+		count[i] += count[i - 1];
+	for (int i = n - 1; i >= 0; i--) {
+		sorted[count[arr[i]] - 1] = arr[i];
+		count[arr[i]]--;
+	}
+	for (int i = 0; i < n; i++)
+		arr[i] = sorted[i];
+
+	free(count);
+	free(sorted);
 }
