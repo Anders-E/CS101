18b559d2ff1973685b60383673abb1d17aa76463
Anders Eriksson <aeri3@kth.se>
2020-01-16 01:31:19 +0100

    add quicksort

--- include/algorithms/sorting.h
+++ include/algorithms/sorting.h
@@ -5,5 +5,6 @@ void bubble_sort(int arr[], int n);
 void counting_sort(int arr[], int n, int k);
 void insertion_sort(int arr[], int n);
 void merge_sort(int arr[], int n);
+void quicksort(int arr[], int n);
 
 #endif // !SORTING_H
---
+++ src/algorithms/sorting/quicksort.c
@@ -0,0 +1,36 @@
+#include <sorting.h>
+
+void swap(int *x, int *y)
+{
+	int tmp = *x;
+	*x = *y;
+	*y = tmp;
+}
+
+int partition(int arr[], int p, int r)
+{
+	int x = arr[r];
+	int i = p - 1;
+	for (int j = p; j < r; j++) {
+		if (arr[j] <= x) {
+			i++;
+			swap(&arr[i], &arr[j]);
+		}
+	}
+	swap(&arr[i + 1], &arr[r]);
+	return i + 1;
+}
+
+void quicksort_(int arr[], int p, int r)
+{
+	if (p < r) {
+		int q = partition(arr, p, r);
+		quicksort_(arr, p, q - 1);
+		quicksort_(arr, q + 1, r);
+	}
+}
+
+void quicksort(int arr[], int n)
+{
+	quicksort_(arr, 0, n - 1);
+}
