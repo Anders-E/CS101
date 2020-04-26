1d9a58b75f009728ac083d062a94f8795a1373f9
Anders Eriksson <aeri3@kth.se>
2020-04-26 23:37:37 +0200

    add unit tests for linked_list

---
+++ test/data_structures/linked_list_test.cpp
@@ -0,0 +1,78 @@
+#include <gtest/gtest.h>
+extern "C" {
+	#include <CS101/linked_list.h>
+}
+
+TEST(LinkedList, NewSuccess)
+{
+	struct linked_list *list = linked_list_new();
+	ASSERT_NE(list, nullptr);
+	linked_list_free(list);
+}
+
+TEST(LinkedList, AppendAndGet)
+{
+	struct linked_list *list = linked_list_new();
+	linked_list_append(list, 1);
+	linked_list_append(list, 2);
+	EXPECT_EQ(linked_list_get(list, 0), 1);
+	EXPECT_EQ(linked_list_get(list, 1), 2);
+	linked_list_free(list);
+}
+
+TEST(LinkedList, Remove)
+{
+	struct linked_list *list = linked_list_new();
+	linked_list_append(list, 1);
+	linked_list_append(list, 2);
+	linked_list_append(list, 3);
+	linked_list_append(list, 4);
+	linked_list_remove(list, 0);
+	linked_list_remove(list, 2);
+	EXPECT_EQ(linked_list_get(list, 0), 2);
+	EXPECT_EQ(linked_list_get(list, 1), 3);
+	ASSERT_EXIT(
+		linked_list_remove(list, 2),
+		::testing::KilledBySignal(SIGSEGV),
+		".*"
+	);
+	linked_list_free(list);
+}
+
+TEST(LinkedList, Set)
+{
+	struct linked_list *list = linked_list_new();
+	linked_list_append(list, 1);
+	linked_list_append(list, 2);
+	linked_list_set(list, 99, 0);
+	linked_list_set(list, -1, 1);
+	EXPECT_EQ(linked_list_get(list, 0), 99);
+	EXPECT_EQ(linked_list_get(list, 1), -1);
+	ASSERT_EXIT(
+		linked_list_set(list, 1, 2),
+		::testing::KilledBySignal(SIGSEGV),
+		".*"
+	);
+	linked_list_free(list);
+}
+
+TEST(LinkedList, Insert)
+{
+	struct linked_list *list = linked_list_new();
+	linked_list_append(list, 1);
+	linked_list_append(list, 2);
+	linked_list_insert(list, 3, 0);
+	linked_list_insert(list, 4, 2);
+	linked_list_insert(list, 5, 4);
+	EXPECT_EQ(linked_list_get(list, 0), 3);
+	EXPECT_EQ(linked_list_get(list, 1), 1);
+	EXPECT_EQ(linked_list_get(list, 2), 4);
+	EXPECT_EQ(linked_list_get(list, 3), 2);
+	EXPECT_EQ(linked_list_get(list, 4), 5);
+	ASSERT_EXIT(
+		linked_list_insert(list, 99, 6),
+		::testing::KilledBySignal(SIGSEGV),
+		".*"
+	);
+	linked_list_free(list);
+}
